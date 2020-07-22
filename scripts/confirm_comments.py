8e0c79447a9c55c833bc273d190663b5843086de
ericlau00 <ericlau41802@gmail.com>
2020-07-22 15:18:01 -0400

    Implement comment addition and locking

--- scripts/confirm_comments.py
+++ scripts/confirm_comments.py
@@ -15,8 +15,27 @@ thread = reddit.submission(id=current['CURRENT_THREAD'])
 
 thread.comments.replace_more(limit=None)
 
+comments = list()
 comment_queue = thread.comments[:]
 while comment_queue:
     comment = comment_queue.pop(0)
-    print(comment.body, datetime.fromtimestamp(comment.created_utc), datetime.now())
+    # TODO: if the comment is in the list of added trades, do not do the next two lines
+    comments.append(comment)
     comment_queue[0:0] = comment.replies
+
+for comment in comments:
+    if not comment.is_root and comment.body == 'Confirmed':
+        parent = comment.parent()
+        while not parent.is_root:
+            parent = comment.parent()
+        if f'u/{comment.author.name}' in parent.body:
+            try:
+                reply = comment.reply('Added!')
+                reply.mod.lock()
+            except:
+                print('COULD NOT REPLY TO LOCKED COMMENT')
+            queue = [parent]
+            while queue:
+                c = queue.pop(0)
+                c.mod.lock()
+                queue[0:0] = c.replies
