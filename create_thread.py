4ce489487e0a007bbb1f380b0cebf0c0ada7e28a
ericlau00 <ericlau41802@gmail.com>
2020-07-22 13:19:37 -0400

    Create functions for create_thread

--- create_thread.py
+++ create_thread.py
@@ -6,31 +6,52 @@ import praw
 
 filepath = os.path.dirname(os.path.abspath(__file__))
 
-f = open(f"{filepath}/config.json")
-config = json.load(f)
-f.close()
+
+def get_json(path):
+    f = open(path)
+    json_dict = json.load(f)
+    f.close()
+    return json_dict
+
+
+def get_subreddit(subreddit):
+    reddit = praw.Reddit(
+        client_id=config['CLIENT_ID'],
+        client_secret=config['CLIENT_SECRET'],
+        user_agent=config['USER_AGENT'],
+        username=config['USERNAME'],
+        password=config['PASSWORD']
+    )
+
+    return reddit.subreddit(subreddit)
+
+
+def create_review_thread(subreddit):
+    thread_title = f"{today.strftime('%B %Y')} Confimred Trade Thread"
+    thread_text = """Post your confirmed trades below!
+
+    When confirming a post, only write 'Confirmed'
+    """
+
+    thread = subreddit.submit(
+        thread_title,
+        selftext=thread_text)
+
+    thread.mod.distinguish(how="yes")
+    thread.mod.sticky()
+    thread.mod.flair('Review Thread')
+
+    return thread
+
+
+config = get_json(f"{filepath}/config.json")
+current_thread = get_json(f"{filepath}/current-thread.json")
 
 today = datetime.now()
 
-thread_title = f"{today.strftime('%B %Y')} Confimred Trade Thread"
-thread_text = """Post your confirmed trades below!
+subreddit = get_subreddit('RHBST')
 
-When confirming a post, only write 'Confirmed'
-"""
-
-reddit = praw.Reddit(
-    client_id=config['CLIENT_ID'],
-    client_secret=config['CLIENT_SECRET'],
-    user_agent=config['USER_AGENT'],
-    username=config['USERNAME'],
-    password=config['PASSWORD']
-)
-
-subreddit = reddit.subreddit('RHBST')
-
-thread = subreddit.submit(
-    thread_title,
-    selftext=thread_text)
+thread = create_review_thread(subreddit)
 
 # TODO: Hook this up to a CRON job and set the current_thread
 # in the database to be the thread.id
