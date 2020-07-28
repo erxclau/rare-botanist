40cf827170a52549e68f3cb3c1a8adb5411e7796
ericlau00 <ericlau41802@gmail.com>
2020-07-28 13:14:41 -0400

    Add update directory script

---
+++ scripts/update_directory.py
@@ -0,0 +1,28 @@
+from utl import utility
+
+config = utility.get_json("config.json")
+data = utility.get_json('comment-data.json')
+
+reddit, subreddit = utility.get_reddit(config)
+
+content = '''This is a comprehensive list of everyone who has traded and/or sold on r/RareHouseplantsBST.
+Use Ctrl + F to find `u/username`.
+'''
+
+table_header = '''|Type|Comments|
+|:---:|:---|
+'''
+
+keys = sorted(list(data.keys()), key=str.lower)
+
+for user in keys:
+    content += f'## {user}\n'
+    content += table_header
+    for interaction in data[user]['history']:
+        content += f"|{interaction['type']}|{interaction['link']}|\n"
+    content += '\n'
+
+subreddit.wiki['userdirectory'].edit(
+    content,
+    'Update user directory'
+)
