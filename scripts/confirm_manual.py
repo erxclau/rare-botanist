8e21c3a850e6c02fa70de70493b85de23fd7cac0
erxclau <ericlau41802@gmail.com>
2021-07-23 20:36:28 -0400

    Confirm sale

--- scripts/confirm_manual.py
+++ scripts/confirm_manual.py
@@ -95,7 +95,7 @@ def validate_trade(comment, parent):
     update_interactions(text, parent, comment)
 
 
-child = reddit.comment(id="h5wwxlf")
+child = reddit.comment(id="h62a1lj")
 parent = get_parent(child)
 
 validate_trade(child, parent)
