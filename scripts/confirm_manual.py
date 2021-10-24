29a9b5c9930808bc93d73b033282f4c7031b9c4e
erxclau <ericlau41802@gmail.com>
2021-10-24 15:09:46 -0400

    Confirm interactions

--- scripts/confirm_manual.py
+++ scripts/confirm_manual.py
@@ -95,7 +95,7 @@ def validate_trade(comment, parent):
     update_interactions(text, parent, comment)
 
 
-child = reddit.comment(id="hh8lke9")
+child = reddit.comment(id="hhoj8ze")
 parent = get_parent(child)
 
 validate_trade(child, parent)
